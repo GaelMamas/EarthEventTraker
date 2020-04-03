a20bd5f13acc289b212620fae0f6551357688ff7
Gaël <mviviengael@gmail.com>
2020-04-03 14:15:31 +0200

    connection to nasa eonet api

--- app/src/main/java/com/villejuif/eartheventtraker/network/EonetEvent.kt
+++ app/src/main/java/com/villejuif/eartheventtraker/network/EonetEvent.kt
@@ -1,7 +1,2 @@
-package com.villejuif.eartheventtraker.network
+package com.villejuif.eartheventtraker.network 
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class EonetEvent(val title:String): Parcelable
